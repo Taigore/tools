cfbea018d97fac7181c8d5cc8bb71de4ddf93171
Federico Bellia <f.bellia@hotmail.it>
2023-05-09 00:53:15 +0200

    Use new program args class in the main

--- Febe.ConfigFilter/Program.cs
+++ Febe.ConfigFilter/Program.cs
@@ -1,29 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Febe.ConfigFilter;
+using Febe.ConfigFilter.Cli;
 
-var action = args.FirstOrDefault();
-if (string.IsNullOrEmpty(action))
-{
-    Console.WriteLine("Call the program with argument clean or smudge");
-    return -1;
-}
-
-var invalidArgsFound = false;
-var configSource = (string?)null;
-for (int i = 1; i < args.Length; ++i)
-{
-    switch (args[i])
-    {
-        case "--config":
-            configSource = args[++i];
-            continue;
-        default:
-            Console.WriteLine("Unrecognized argument: {0}", args[i]);
-            invalidArgsFound = true;
-            continue;
-    }
-}
-if (invalidArgsFound)
+var programArgs = ProgramArgs.Parse(args);
+if (programArgs == null)
 {
     return -1;
 }
@@ -32,25 +12,25 @@ var filter = new Filter(
     Console.OpenStandardInput(),
     Console.OpenStandardOutput()
 );
-if (action == "clean")
+
+if (programArgs.Action == "clean")
 {
     filter.Clean();
     return 0;
 }
-else if (action == "smudge")
+
+if (programArgs.Action == "smudge")
 {
-    if(string.IsNullOrEmpty(configSource))
+    var configPath = programArgs.ConfigPath;
+    if (string.IsNullOrEmpty(configPath))
     {
         Console.WriteLine("Define a source for the values by passing the argument --config \"path\"");
         return -1;
     }
 
-    var configLoader = new ConfigLoader(configSource);
+    var configLoader = new ConfigLoader(configPath);
     filter.Smudge(configLoader);
     return 0;
 }
-else
-{
-    Console.WriteLine("Call the program with argument clean or smudge");
-    return -1;
-}
+
+return 0;
