c4f49fcabdc19762f1daad65899d3f9d71a738e7
Federico Bellia <f.bellia@hotmail.it>
2023-05-09 00:46:11 +0200

    Add argument parsing for action

--- Febe.ConfigFilter/Cli/ProgramArgs.cs
+++ Febe.ConfigFilter/Cli/ProgramArgs.cs
@@ -6,8 +6,24 @@
     using System.Text;
     using System.Threading.Tasks;
 
-    internal class ProgramArgs
+    public class ProgramArgs
     {
+        public string Action { get; }
 
+        public static ProgramArgs? Parse(string[] args)
+        {
+            var action = args[0];
+            if(action != "clean" && action != "smudge")
+            {
+                return null;
+            }
+
+            return new ProgramArgs(action);
+        }
+
+        public ProgramArgs(string action)
+        {
+            Action = action;
+        }
     }
 }
